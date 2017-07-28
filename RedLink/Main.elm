0062cafd1f05260d5baf1924d3c7759f26feeb4a
Beeno Tung <aabbcc1241@yahoo.com.hk>
2017-07-28 21:18:58 +0800

    added menu layout; added close menu button;

--- RedLink/Main.elm
+++ RedLink/Main.elm
@@ -1,20 +1,25 @@
 module Main exposing (..)
 
-import Html exposing (Html, body, div, h1, node, span, text)
+import Html exposing (Html, body, div, h1, li, node, span, text, ul)
 import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
+import Svg exposing (svg)
+import Svg.Attributes
 
 
 type alias Model =
-    {}
+    { isShowMenu : Bool
+    }
 
 
 type Msg
-    = Msg
+    = ShowMenu
+    | CloseMenu
 
 
 initModel : Model
 initModel =
-    {}
+    { isShowMenu = False }
 
 
 init =
@@ -23,21 +28,32 @@ init =
 
 update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
-    model ! []
+    case msg of
+        ShowMenu ->
+            { model | isShowMenu = True } ! []
+
+        CloseMenu ->
+            { model | isShowMenu = False } ! []
 
 
 view : Model -> Html Msg
 view model =
     node "app"
         [ appStyle ]
-        [ viewHeader model
-        ]
+        (if model.isShowMenu then
+            [ viewHeader model
+            , viewMenu model
+            ]
+         else
+            [ viewHeader model
+            ]
+        )
 
 
 appStyle =
     style
         [ ( "display", "block" )
-        , ( "padding", "8px" )
+          --, ( "padding", "8px" )
           --, ( "width", "100%" )
           --, ( "height", "100%" )
         ]
@@ -47,7 +63,25 @@ viewHeader model =
     node "header"
         [ headerStyle ]
         [ headerText "Red-Link"
-        , menuButton model
+        , let
+            theStyle =
+                style
+                    [ ( "display", "block" )
+                    , ( "float", "right" )
+                    ]
+          in
+            if model.isShowMenu then
+                node "cross"
+                    [ theStyle
+                    , onClick CloseMenu
+                    ]
+                    [ crossSvg ]
+            else
+                node "menuButton"
+                    [ theStyle
+                    , onClick ShowMenu
+                    ]
+                    menuLines
         ]
 
 
@@ -57,30 +91,76 @@ headerText content =
             [ ( "color", "white" )
             , ( "font-weight", "bold" )
             , ( "font-family", "sans-serif" )
+            , ( "line-height", "28px" )
+            , ( "vertical-align", "center" )
             ]
         ]
         [ text content ]
 
 
 headerStyle =
-    style [ ( "background-color", "red" ) ]
+    style
+        [ ( "background-color", "red" )
+        , ( "display", "block" )
+        , ( "height", "28px" )
+        , ( "padding-left", "4px" )
+        ]
 
 
-menuButton model =
-    node "menuButton"
-        [ style [ ( "background-color", "pink" ) ] ]
-        (List.repeat 3
-            (div
-                [ style
-                    [ ( "margin", "4px 8px" )
-                    , ( "background-color", "white" )
-                    , ( "width", "20px" )
-                    , ( "height", "4px" )
-                    ]
+crossSvg =
+    svg
+        [ Svg.Attributes.width "28"
+        , Svg.Attributes.height "28"
+        , Svg.Attributes.viewBox "0 0 28 28"
+        ]
+        [ Svg.line
+            [ Svg.Attributes.fill "none"
+            , Svg.Attributes.stroke "white"
+            , Svg.Attributes.strokeWidth "4"
+            , Svg.Attributes.x1 "4"
+            , Svg.Attributes.y1 "4"
+            , Svg.Attributes.x2 "24"
+            , Svg.Attributes.y2 "24"
+            ]
+            []
+        , Svg.line
+            [ Svg.Attributes.fill "none"
+            , Svg.Attributes.stroke "white"
+            , Svg.Attributes.strokeWidth "4"
+            , Svg.Attributes.x1 "4"
+            , Svg.Attributes.y1 "24"
+            , Svg.Attributes.x2 "24"
+            , Svg.Attributes.y2 "4"
+            ]
+            []
+        ]
+
+
+menuLines =
+    List.repeat 3
+        (div
+            [ style
+                [ ( "margin", "4px 8px" )
+                , ( "background-color", "white" )
+                , ( "width", "20px" )
+                , ( "height", "4px" )
                 ]
-                []
-            )
+            ]
+            []
         )
+
+
+viewMenu model =
+    node "menu"
+        []
+        [ ul [ style [ ( "list-style-type", "none" ) ] ]
+            [ li [] [ text "Fate Angle" ]
+            , li [] [ text "Love Formula" ]
+            , li [] [ text "Secrete Wall" ]
+            , li [] [ text "Draw & Guess" ]
+            , li [] [ text "Events" ]
+            ]
+        ]
 
 
 subscriptions : Model -> Sub Msg
